a78f34d1470d0c51028187210b2bbfec2163b2d1
RahulCodeTiwari <rahul030704@gmail.com>
2025-07-22 09:07:56 +0530

    update server

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(clerkMiddleware())
 // API to listen to Clerk Webhooks
 app.use("/api/clerk", clerkWebhooks);
 
-app.get('/', (req, res)=> res.send("API is working"))
+app.get('/', (req, res)=> { res.send("API is working");});
 
 
 const PORT = process.env.PORT || 3000;
