6a4f37713436b6ea22bd785be5917006bec68077
RahulCodeTiwari <rahul030704@gmail.com>
2025-07-31 18:31:57 +0530

    connection backend with frontend

--- server/server.js
+++ server/server.js
@@ -6,9 +6,13 @@ import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from './controllers/clerkWebhooks.js';
 import userRouter from './routes/userRoutes.js';
 import hotelRouter from './routes/hotelRoutes.js';
+import roomRouter from './routes/roomRoutes.js';
+import connectCLoudinary from './configs/cloudinary.js';
+import bookingRouter from './routes/bookingRoutes.js';
 
 
 connectDB()
+connectCLoudinary();
 
 const app = express()
 app.use(cors())   // Enable CORS for all routes
@@ -24,6 +28,8 @@ app.use("/api/clerk", clerkWebhooks);
 app.get('/', (req, res)=> { res.send("API is working");});
 app.use("/api/user", userRouter)
 app.use('/api/hotels', hotelRouter)
+app.use('/api/rooms', roomRouter)
+app.use('/api/bookings', bookingRouter)
 
 
 
